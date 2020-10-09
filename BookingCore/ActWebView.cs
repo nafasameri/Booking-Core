f579b7ac653107455efcfd5455bb6511d6bc97fb
nafasameri <engineer.nafas.ameri.1997@gmail.com>
2020-10-09 20:02:25 +0330

    update

---
+++ BookingCore/ActWebView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Webkit;
+
+
+namespace BookingCore
+{
+    class ActWebView : WebViewClient
+    {
+        public override bool ShouldOverrideUrlLoading(WebView view, string url)
+        {
+            base.ShouldOverrideUrlLoading(view, url);
+            view.LoadUrl(url);
+            return true;
+        }
+
+        public override void OnPageFinished(WebView view, string url)
+        {
+            base.OnPageFinished(view, url);
+            MainActivity.SetVisibility(ViewStates.Gone);
+        }
+    }
+}
--- BookingCore/MainActivity.cs
+++ BookingCore/MainActivity.cs
@@ -4,39 +4,70 @@ using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
+using Android.Webkit;
 using Android.Widget;
 
 namespace BookingCore
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, Icon = "@mipmap/favicon")]
     public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
     {
-        TextView textMessage;
+        private WebView webView;
+        private static ProgressBar progressBar;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            textMessage = FindViewById<TextView>(Resource.Id.message);
+            webView = FindViewById<WebView>(Resource.Id.webView);
+            progressBar = FindViewById<ProgressBar>(Resource.Id.prb_webview);
+
             BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
             navigation.SetOnNavigationItemSelectedListener(this);
+
+            webView.LoadUrl("http://hafashta.ir");
+            webView.Settings.JavaScriptEnabled = true;
+            webView.SetWebViewClient(new ActWebView());
         }
+
         public bool OnNavigationItemSelected(IMenuItem item)
         {
             switch (item.ItemId)
             {
                 case Resource.Id.navigation_home:
-                    textMessage.SetText(Resource.String.title_home);
+                    progressBar.Visibility = ViewStates.Visible;
+                    webView.LoadUrl("http://hafashta.ir");
+                    webView.Settings.JavaScriptEnabled = true;
+                    webView.SetWebViewClient(new ActWebView());
                     return true;
                 case Resource.Id.navigation_dashboard:
-                    textMessage.SetText(Resource.String.title_dashboard);
+                    progressBar.Visibility = ViewStates.Visible;
+                    webView.LoadUrl("http://hafashta.ir/login");
+                    webView.Settings.JavaScriptEnabled = true;
+                    webView.SetWebViewClient(new ActWebView());
                     return true;
                 case Resource.Id.navigation_notifications:
-                    textMessage.SetText(Resource.String.title_notifications);
+                    progressBar.Visibility = ViewStates.Visible;
+                    webView.LoadUrl("http://hafashta.ir:2222");
+                    webView.Settings.JavaScriptEnabled = true;
+                    webView.SetWebViewClient(new ActWebView());
                     return true;
             }
             return false;
+        }
+
+        public override void OnBackPressed()
+        {
+            if (webView.CanGoBack())
+                webView.GoBack();
+            else
+                base.OnBackPressed();
+        }
+
+        public static void SetVisibility(ViewStates viewStates)
+        {
+            progressBar.Visibility = viewStates;
         }
     }
 }
